d3166eea0c78d02fdabbd8af6e3defb04f75c68e
Andrew Luca <thendrluca@gmail.com>
2020-03-22 11:14:30 +0200

    feat: add react suspense, move provider up in tree

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { IntlProvider } from 'react-intl';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { IntlProvider } from "react-intl";
+import { Provider } from "react-redux";
 // if needed we can partial import
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from './App';
-import messages_ro from './i18n/ro.json';
-import messages_ru from './i18n/ru.json';
+import App from "./App";
+import messages_ro from "./i18n/ro.json";
+import messages_ru from "./i18n/ru.json";
+import store from "./store";
 
-import './index.css';
+import "./index.css";
 
-const language = 'ro';
+const language = "ro";
 
 const messages = {
-  'ro': messages_ro,
-  'ru': messages_ru
-}
+  ro: messages_ro,
+  ru: messages_ru
+};
 
 ReactDOM.render(
   <IntlProvider locale={language} messages={messages[language]}>
-    <App />
-  </IntlProvider>
-, document.getElementById('root'));
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </IntlProvider>,
+  document.getElementById("root")
+);
