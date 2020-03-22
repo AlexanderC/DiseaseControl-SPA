5c9ecbef31f427f37bd6d0b9e592c78d034a0e6f
Andrew Luca <thendrluca@gmail.com>
2020-03-22 17:51:37 +0200

    feat: show intory updated dates, replace button with link (#11)

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,17 +1,34 @@
 import React, { useEffect, useCallback, useState } from "react";
-import { Link } from 'react-router-dom';
-import * as T from '../../resources/types';
+import { Link } from "react-router-dom";
+import * as T from "../../resources/types";
 import { useSelector, useDispatch } from "react-redux";
-import { getHospitals, getTags } from '../../actions/DataActions';
-import { Container, Row, Col, Card, CardTitle, CardText, ListGroup, ListGroupItem, Badge, CardBody, Button } from "reactstrap";
-import { selectHospitals, selectTags, selectTagColors } from "../../reducers/Combiner";
-import Tags from '../common/tags/Tags';
+import { getHospitals, getTags } from "../../actions/DataActions";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardTitle,
+  CardText,
+  ListGroup,
+  ListGroupItem,
+  Badge,
+  CardBody,
+  Button,
+  CardSubtitle,
+} from "reactstrap";
+import {
+  selectHospitals,
+  selectTags,
+  selectTagColors,
+} from "../../reducers/Combiner";
+import { Tags } from "../common/tags/Tags";
 
-function Dashboard() {
+export function Dashboard() {
   const hospitals = useSelector(selectHospitals);
   const tags = useSelector(selectTags);
   const tagColors = useSelector(selectTagColors);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const [selectedTags, setSelectedTags] = useState<T.Tag[]>([]);
   const [filteredData, setFilteredData] = useState<T.Hospital[]>([]);
@@ -19,93 +36,95 @@ function Dashboard() {
   useEffect(() => {
     dispatch(getHospitals());
     dispatch(getTags());
-  }, [dispatch])
+  }, [dispatch]);
 
   useEffect(() => {
     setFilteredData(hospitals);
   }, [hospitals]);
 
-  const handleTagClick = useCallback((tag: T.Tag) => {
-    let newSelectedTags = selectedTags;
-    if (selectedTags.includes(tag)) {
-      newSelectedTags = newSelectedTags.filter(t => t.id !== tag.id);
-    } else {
-      newSelectedTags.push(tag);
-    }
+  const handleTagClick = useCallback(
+    (tag: T.Tag) => {
+      let newSelectedTags = selectedTags;
+      if (selectedTags.includes(tag)) {
+        newSelectedTags = newSelectedTags.filter((t) => t.id !== tag.id);
+      } else {
+        newSelectedTags.push(tag);
+      }
 
-    const selectedTagIds = newSelectedTags.map(t => t.id);
-    let newFilteredData;
-    if (selectedTagIds.length) {
-      newFilteredData = hospitals.filter(d =>
-        selectedTagIds.every(id => d.tags.map(t => t.id).includes(id))
-      );
-    } else {
-      newFilteredData = hospitals;
-    }
+      const selectedTagIds = newSelectedTags.map((t) => t.id);
+      let newFilteredData;
+      if (selectedTagIds.length) {
+        newFilteredData = hospitals.filter((d) =>
+          selectedTagIds.every((id) => d.tags.map((t) => t.id).includes(id))
+        );
+      } else {
+        newFilteredData = hospitals;
+      }
 
-    setSelectedTags(newSelectedTags);
-    setFilteredData(newFilteredData);
-  }, [selectedTags, hospitals]);
+      setSelectedTags(newSelectedTags);
+      setFilteredData(newFilteredData);
+    },
+    [selectedTags, hospitals]
+  );
 
   return (
     <Container className="my-3">
-      <Row className="ml-1 mb-2">
-        {
-          tags.map(t => {
-            const isSelected = selectedTags.includes(t);
-            return (
-              <Button
-                key={t.id}
-                outline
-                color={tagColors[t.id]}
-                active={isSelected}
-                className="m-1"
-                onClick={() => handleTagClick(t)}
-              >
-                {t.description}
-              </Button>
-            )
-          })
-        }
-      </Row>
+      <div className="mb-2">
+        {tags.map((t) => {
+          const isSelected = selectedTags.includes(t);
+          return (
+            <Button
+              key={t.id}
+              outline
+              color={tagColors[t.id]}
+              active={isSelected}
+              className="mr-1 mb-1"
+              onClick={() => handleTagClick(t)}
+            >
+              {t.description}
+            </Button>
+          );
+        })}
+      </div>
       <Row>
-        {filteredData.map(d => (
-          <Col key={d.id} xs="12" sm="6" md="4" lg="3" className="mb-4">
-            <Card className="h-100">
-              <CardBody>
-                <CardTitle tag="h5">
-                  {sliceStringAndAppendDots(d.name, 35)}
-                </CardTitle>
-                <CardText>
-                  {sliceStringAndAppendDots(d.description, 100)}
-                </CardText>
-                <Tags data={d.tags}/>
-                <Button color="primary" tag={Link} to={`/details/${d.id}`}>
-                  Details
-                </Button>
-              </CardBody>
-              <ListGroup flush className="mt-auto">
-                {d.inventory.map(p => (
-                  <ListGroupItem key={p.id} className="d-flex justify-content-between align-items-center">
-                    {p.name}
-                    <Badge pill>{p.HospitalInventory.quantity}</Badge>
-                  </ListGroupItem>
-                ))}
-              </ListGroup>
-            </Card>
-          </Col>
-        ))}
+        {filteredData.map((d) => {
+          const updatedAt = new Date(d.updatedAt);
+          return (
+            <Col key={d.id} xs="12" sm="6" md="6" lg="3" className="mb-4">
+              <Card className="h-100">
+                <CardBody>
+                  <CardTitle tag="h4">
+                    <Link to={`/details/${d.id}`}>{truncate(d.name, 35)}</Link>
+                  </CardTitle>
+                  <CardSubtitle tag="small" className="text-muted">
+                    {updatedAt.toLocaleDateString()} -{" "}
+                    {updatedAt.toLocaleTimeString()}
+                  </CardSubtitle>
+                  <CardText>
+                    {truncate(d.description, 100)}
+                    <Tags data={d.tags} />
+                  </CardText>
+                </CardBody>
+                <ListGroup flush className="mt-auto">
+                  {d.inventory.map((p) => (
+                    <ListGroupItem
+                      key={p.id}
+                      className="d-flex justify-content-between align-items-center"
+                    >
+                      {p.name}
+                      <Badge pill>{p.HospitalInventory.quantity}</Badge>
+                    </ListGroupItem>
+                  ))}
+                </ListGroup>
+              </Card>
+            </Col>
+          );
+        })}
       </Row>
     </Container>
-  )
+  );
 }
 
-export default Dashboard;
-
-function sliceStringAndAppendDots(str: string, sliceAt = 15) {
-  let slicedString = str || '';
-  if (slicedString.length > sliceAt) {
-    slicedString = `${slicedString.slice(0, sliceAt)}...`;
-  }
-  return slicedString;
+function truncate(text: string = "", sliceAt = 15) {
+  return text.length > sliceAt ? `${text.slice(0, sliceAt)}...` : text;
 }
--- src/components/details/Details.tsx
+++ src/components/details/Details.tsx
@@ -7,7 +7,7 @@ import {
 } from "../../actions/DataActions";
 import { selectHospitals } from "../../reducers/Combiner";
 import { Container, ListGroup, ListGroupItem, Badge, Button } from "reactstrap";
-import Tags from "../common/tags/Tags";
+import { Tags } from "../common/tags/Tags";
 
 function Details(props: RouteComponentProps<{ id: string }>) {
   const { match } = props;
@@ -38,9 +38,15 @@ function Details(props: RouteComponentProps<{ id: string }>) {
 
   if (!hospital) return null;
 
+  const updatedAt = new Date(hospital.updatedAt);
+
   return (
     <Container className="my-3">
       <h1>{hospital.name}</h1>
+      <small className="text-muted">
+        Last updated: {updatedAt.toLocaleDateString()} -{" "}
+        {updatedAt.toLocaleTimeString()}
+      </small>
       <p>{hospital.description}</p>
       <Tags data={hospital.tags} />
 
@@ -50,44 +56,53 @@ function Details(props: RouteComponentProps<{ id: string }>) {
         {hospital.inventory.length === 0 && (
           <ListGroupItem>No items</ListGroupItem>
         )}
-        {hospital.inventory.map((inventory) => (
-          <ListGroupItem
-            key={inventory.id}
-            className="d-flex justify-content-between align-items-center"
-          >
-            {inventory.name} ({new Date(inventory.updatedAt).toDateString()})
-            <div>
-              <Button
-                size="sm"
-                color="link"
-                className="mr-1"
-                onClick={() =>
-                  updateInventItemCount(
-                    inventory.HospitalInventory.id,
-                    inventory.HospitalInventory.quantity + 1
-                  )
-                }
-              >
-                <span>+</span>
-              </Button>
-              <Button
-                size="sm"
-                color="link"
-                className="mr-1"
-                disabled={inventory.HospitalInventory.quantity === 0}
-                onClick={() =>
-                  updateInventItemCount(
-                    inventory.HospitalInventory.id,
-                    inventory.HospitalInventory.quantity - 1
-                  )
-                }
-              >
-                <span>-</span>
-              </Button>
-              <Badge>{inventory.HospitalInventory.quantity}</Badge>
-            </div>
-          </ListGroupItem>
-        ))}
+        {hospital.inventory.map((inventory) => {
+          const inventUpdatedAt = new Date(inventory.updatedAt);
+          return (
+            <ListGroupItem
+              key={inventory.id}
+              className="d-flex justify-content-between align-items-center"
+            >
+              <div>
+                <div>{inventory.name}</div>
+                <small className="text-muted">
+                  Last updated: {inventUpdatedAt.toLocaleDateString()} -{" "}
+                  {inventUpdatedAt.toLocaleTimeString()}
+                </small>
+              </div>
+              <div>
+                <Button
+                  size="sm"
+                  color="link"
+                  className="mr-1"
+                  onClick={() =>
+                    updateInventItemCount(
+                      inventory.HospitalInventory.id,
+                      inventory.HospitalInventory.quantity + 1
+                    )
+                  }
+                >
+                  <span>+</span>
+                </Button>
+                <Button
+                  size="sm"
+                  color="link"
+                  className="mr-1"
+                  disabled={inventory.HospitalInventory.quantity === 0}
+                  onClick={() =>
+                    updateInventItemCount(
+                      inventory.HospitalInventory.id,
+                      inventory.HospitalInventory.quantity - 1
+                    )
+                  }
+                >
+                  <span>-</span>
+                </Button>
+                <Badge>{inventory.HospitalInventory.quantity}</Badge>
+              </div>
+            </ListGroupItem>
+          );
+        })}
       </ListGroup>
 
       <h4>History</h4>
