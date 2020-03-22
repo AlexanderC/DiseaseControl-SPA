211e68486eef4e520d644ac23054949f2d2cb269
Nicoleta Iliuha <niliuha@fusionworks.md>
2020-03-22 10:51:55 +0200

    fix for hospitals query

--- src/actions/DataActions.js
+++ src/actions/DataActions.js
@@ -2,8 +2,10 @@ import axios from '../services/Axios';
 import * as T from './types';
 
 export function getHospitals() {
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
   return function (dispatch) {
-    return axios.get('/hospital').then(({data }) => {
+    return axios.get(`/hospital?token=${currentUser.token}`).then(({data }) => {
       dispatch({
         type: T.GET_HOSPITAL_DATA,
         payload: data
