beef41f62e367a3d060d633e00af827b0e1f1413
Andrew Luca <thendrluca@gmail.com>
2020-03-26 10:35:44 +0200

    fix(footer): return the love

--- src/shared/footer.component.tsx
+++ src/shared/footer.component.tsx
@@ -6,17 +6,31 @@ export function Footer() {
   const l10n = useFormatMessage();
   return (
     <section className="mt-auto p-4 text-center bg-light">
-      <Badge
-        tag="a"
-        color="warning"
-        href="https://c19.md"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="mr-2"
-      >
+      <Badge tag="a" color="warning" href="https://c19.md" target="_blank" rel="noopener noreferrer" className="mr-2">
         c19.md
       </Badge>
-      {l10n("footer")}
+      Instrument open-source în contextul pandemiei Covid-19 realizat cu ❤ de Moldoveni p-tru Moldoveni. Cod sursa{" "}
+      <Badge
+        tag="a"
+        color="info"
+        href="https://github.com/AlexanderC/DiseaseControl"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="mr-1"
+      >
+        &lt; aici /&gt;
+      </Badge>{" "}
+      si{" "}
+      <Badge
+        tag="a"
+        color="info"
+        href="https://github.com/AlexanderC/DiseaseControl-SPA"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="mr-1"
+      >
+        &lt; aici /&gt;
+      </Badge>
     </section>
   );
 }
